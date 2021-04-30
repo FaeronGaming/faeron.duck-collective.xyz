92448e39790370660a91f194a32960437e8d89b4
FaeronGaming <animeniac@live.com>
2021-04-30 14:53:57 -0600

    Setup eslint

--- src/Post.tsx
+++ src/Post.tsx
@@ -1,6 +1,6 @@
 import { Post as PostType } from './graphcms';
 
-export const Post : React.FC<PostType> = ({ title, coverImage, date, author, slug, excerpt}) => {
+export const Post : React.FC<PostType> = ({ title, }) => {
   return (
     <section>
       <div>{title}</div>
